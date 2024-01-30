c2066c08f6fb615ba5616f9b8c8cdbf3d5eb27f1
neeraj <nierraj@gmail.com>
2024-01-30 13:35:31 +0400

    Initial Commit

---
+++ src/main/java/com/neeraj/reactive/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.neeraj.reactive.repository;
+
+import com.neeraj.reactive.dto.ProductDto;
+import com.neeraj.reactive.entity.Product;
+import org.springframework.data.domain.Range;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+
+@Repository
+public interface ProductRepository extends ReactiveMongoRepository<Product,String> {
+    Flux<ProductDto> findByPriceBetween(Range<Double> priceRange);
+}
